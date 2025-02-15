396a9fc27fed4ed0be3b21c5d1afaeced039d84f
vboxuser <vboxuser@xubuntuu.myguest.virtualbox.org>
2025-02-15 16:24:32 +0530

    added background

--- love.js
+++ love.js
@@ -1,14 +1,24 @@
 let per=Math.floor(Math.random()*100);
 const love=document.getElementById("love")
 const submit=document.getElementById("submit");
+const para=document.getElementById("para");
 submit.addEventListener("click",()=>{
-    if(document.getElementById("fname").value==="" && document.getElementById("fname").value===""){
+    if(document.getElementById("fname").value==="" || document.getElementById("fname").value===""){
         love.textContent="girl fill the nameess"
         } 
         else{
-            love.textContent=`your love percantage ${per}%`
+            love.textContent=`your love percentage ${per}%`
+            if(per<=20){
+                para.textContent="It pains me to say fair maiden/knight ,that your love doth not seem to be destined for greatness.."
+                }
+                else if(per>20 && per <=50){
+                    para.textContent="Verily,your love is a canvas, awaiting the brushstrokes of devotion and affection"
+                }
+                else if(per>50 && per<=90){
+                    para.textContent="Your love doth shine like a radiant star, lighting the way through life's journey"
+                }
+                else{
+                 para.textContent="Your love doth trascend the stars, a celestial bond that shall never break"
+                }
         }
 })
-if(document.getElementById("fname").value!=="" && document.getElementById("fname").value!==""){
-love.textContent=`the love between you guys is ${per}`
-}
