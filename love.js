818ae5376f7dd4d6d2df33fe85c1dc01149149a4
vboxuser <vboxuser@xubuntuu.myguest.virtualbox.org>
2025-02-16 14:05:16 +0530

    made change to same input

--- love.js
+++ love.js
@@ -3,16 +3,23 @@ const love=document.getElementById("love")
 const submit=document.getElementById("submit");
 const para=document.getElementById("para");
 submit.addEventListener("click",()=>{
-    if(document.getElementById("fname").value==="" || document.getElementById("fname").value===""){
+    if(document.getElementById("fname").value==="" || document.getElementById("sname").value===""){
         love.textContent="Kindly inscribe thy names within the empty space"
         } 
+        else if(document.getElementById("fname").value===document.getElementById("sname").value){
+            love.textContent=`Thy heart's numerical score: 100%`;
+            para.textContent="Hark! Thou hast attained perfection in self-love!"
+        }
         else{
             love.textContent=`Thy heart's numerical score: ${per}%`
             if(per<=20){
                 para.textContent="It pains me to say fair maiden/knight ,that your love doth not seem to be destined for greatness.."
                 }
-                else if(per>20 && per <=50){
+                else if(per>20 && per <=30){
                     para.textContent="Verily,your love is a canvas, awaiting the brushstrokes of devotion and affection.."
+                }
+                else if(per>30 && per <=50){
+                    para.textContent="Thy love doth flicker,yet 'tis not lost.Nurture it well, and 'twill grow to a passion most true!"
                 }
                 else if(per>50 && per<=90){
                     para.textContent="Your love doth shine like a radiant star, lighting the way through life's journey.."
@@ -22,3 +29,5 @@ submit.addEventListener("click",()=>{
                 }
         }
 })
+
+
